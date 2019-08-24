2cf80016ca2b5db4bd9162d1d72c92e3a5d73483
lajeff77 <lajeff77@gmail.com>
2019-08-24 14:56:16 -0700

    Initial commit

---
+++ desktop/src/com/jefferson/lauryn/shoot_em_up/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jefferson.lauryn.shoot_em_up.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jefferson.lauryn.shoot_em_up.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
